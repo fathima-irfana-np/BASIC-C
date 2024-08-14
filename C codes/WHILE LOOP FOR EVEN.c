476390e5aeb69c5d358789e85108c4578014b3c1
FATHIMA IRFANA N P <139485504+fathima-irfana-np@users.noreply.github.com>
2024-08-15 00:17:35 +0530

    Add files via upload

---
+++ C codes/WHILE LOOP FOR EVEN.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+
+
+
+int limit,i=2;
+printf("enter the last number");
+scanf("%d",&limit);
+while(i<=limit){
+    if(i%2==0)
+    printf("%d\t",i);
+     i++;
+}
+
+
+
+
+}
